9d9bb7e9be467f147c04db94af3479cbf5eb02b0
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-06-11 20:01:31 +0300

    hw4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { ContactFilter } from './ContactFilter/ContactFilter';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  
+  const deleteContact = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} setContacts={setContacts} />
+      <h2>Contacts</h2>
+      <ContactFilter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={contacts} filter={filter} deleteContact={deleteContact} />
     </div>
   );
 };
